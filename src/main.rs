1e9db9627acedef7fc05522f6f6a3bfa3d2c6a24
rxanders35 <reidxanders@outlook.com>
2025-02-21 21:00:44 -0500

    driver init

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod build;
 
 #[tokio::main]
 async fn main() {
+    #[allow(unused)]
     let cfg = raft::Config::new(0);
+    #[allow(unused)]
     let logger = unimplemented!();
     #[allow(unused)]
     let raft_log = unimplemented!();
-    let node = RawNode::new(&cfg, raft_log, logger);
+    // let node = RawNode::new(&cfg, raft_log, logger);
 }
