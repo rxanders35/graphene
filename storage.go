ace8d982f99112ba9c0d8128e39acc79888aef8c
rxanders35 <reidxanders@outlook.com>
2025-02-22 23:56:46 -0500

    im dumb

--- storage.go
+++ storage.go
@@ -5,35 +5,35 @@ package main
 // NEEDLE: MAGICNUMBER|UUID|DATA|DATA_SIZE|CHECKSUM|SIZE
 
 // Size of Needle's magic number
-var needleMagicSize uint16
+const needleMagicSize = 2
 
 // Size of Needle's UUID
-var needleIDSize [16]byte
+const needleIDSize = 16
 
 // Size of Needle's internal data
-var needleDataSize uint32
+const needleDataSize = 4
 
 // Size of Needle's checksum
-var needleChecksum uint32
+const needleChecksum = 4
 
 // The total size of the Needle
-var needleSizeTotal [26]byte
+const needleSizeTotal = 26
 
 // The Needle magic number literal
-var needleMagicVal = 0xCAFE
+const needleMagicVal = 0xCAFE
 
 /////////////////////////////////////
 
 // CONSTANTS FOR OBJECT INDEX ON DISK
 
 // Object ID field
-var idxObjectID [16]byte
+const idxObjectID = 16
 
 // Offset field
-var idxOffset uint64
+const idxOffset = 8
 
 // Size field
-var idxSize uint32
+const idxSize = 4
 
 // The total size of the Index entry
-var idxSizeTotal [28]byte
+const idxSizeTotal = 28
