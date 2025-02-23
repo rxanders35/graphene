aacb91adc4d229d1dd38be7354d7be9ca6eb6932
rxanders35 <reidxanders@outlook.com>
2025-02-23 00:10:20 -0500

    makeFileName

--- storage.go
+++ storage.go
@@ -1,6 +1,10 @@
 package main
 
+/////////////////////////////////////
+
 // CONSTANTS FOR NEEDLE FORMAT ON DISK
+
+/////////////////////////////////////
 
 // NEEDLE: MAGICNUMBER|UUID|DATA|DATA_SIZE|CHECKSUM|SIZE
 
@@ -26,6 +30,10 @@ const needleMagicVal uint16 = 0xCAFE
 
 // CONSTANTS FOR OBJECT INDEX ON DISK
 
+/////////////////////////////////////
+
+// IDX: OBJECT_ID|OFFSET|SIZE
+
 // Object ID field
 const idxObjectID = 16
 
@@ -37,3 +45,26 @@ const idxSize = 4
 
 // The total size of the Index entry
 const idxSizeTotal = 28
+
+/////////////////////////////////////
+
+// CONSTANTS FOR FILENAME CONSTRAINTS ON DISK
+
+/////////////////////////////////////
+
+// Data file prefix
+const dataFilePrefix = "volume_"
+
+// Index file prefix
+const indexFilePrefix = "volume_"
+
+// Data file suffix
+const dataFileExt = ".dat"
+
+// Index file suffix
+const indexFileExt = ".idx"
+
+/////////////////////////////////////
+
+func makeFileMame(port string) (string, string) {
+}
