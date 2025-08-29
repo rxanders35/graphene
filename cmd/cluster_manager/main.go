c85ba45f7811a85708b7d8dc64d0598bdbe78d44
rxanders35 <reidxanders@outlook.com>
2025-08-29 18:19:52 -0400

    deleted old master

--- cmd/cluster_manager/main.go
+++ cmd/cluster_manager/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"flag"
-	"log"
-
-	"github.com/rxanders35/graphene/pkg/cluster_manager"
-)
-
 func main() {
-	masterAddr := flag.String("master-addr", "localhost:9090", "master's grpc address")
 
-	flag.Parse()
-	log.Printf("Starting")
-
-	s := cluster_manager.NewGRPCServer(*masterAddr)
-	s.Run()
 }
